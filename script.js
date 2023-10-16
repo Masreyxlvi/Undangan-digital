c73fe5d1713098c96f962fe2f9a4536ab0dc4719
Reyhan Tri R <reyhanramadan21@smk.belajar.id>
2023-10-16 09:57:35 +0700

    memperbaiki bug pada navbar

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-// Humberger menu
+// Humberger munu
+
 const stickyTop = document.querySelector(".sticky-top");
 const offcanvas = document.querySelector(".offcanvas");
+const Humberger = document.querySelector(".navbar-toggler");
 
-offcanvas.addEventListener("show.bs.offcanvas", function () {
+Humberger.addEventListener("click", function () {
+  // document.body.style.paddingRight = "400px";
   stickyTop.style.overflow = "visible";
 });
 
 offcanvas.addEventListener("hidden.bs.offcanvas", function () {
+  // document.body.style.paddingRight = "0";
   stickyTop.style.overflow = "hidden";
 });
+
+// DisableScroll
+scrollTop = window.padeYOffset;
