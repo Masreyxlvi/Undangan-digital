5c862da1b01447c15fc636dd8623bb97610096b8
Reyhan Tri R <reyhanramadan21@smk.belajar.id>
2023-10-18 12:06:40 +0700

    penambahan audio & url params

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const stickyTop = document.querySelector(".sticky-top");
 const offcanvas = document.querySelector(".offcanvas");
 const Humberger = document.querySelector(".navbar-toggler");
+const link = document.querySelector(".navbar-nav");
 
 Humberger.addEventListener("click", function () {
   // document.body.style.paddingRight = "400px";
@@ -14,8 +15,19 @@ offcanvas.addEventListener("hidden.bs.offcanvas", function () {
   stickyTop.style.overflow = "hidden";
 });
 
+link.addEventListener("hidden.bs.offcanvas", function () {
+  // alert("test");
+  // document.body.style.paddingRight = "0";
+  stickyTop.style.overflow = "hidden";
+});
+
 // DisableScroll
 const rootElememt = document.querySelector(":root");
+const audioIcon = document.querySelector(".audio-icon-wrapper");
+let isPlaying = false;
+const song = document.querySelector("#song");
+const icon = document.querySelector(".audio-icon-wrapper i");
+
 function DisableScroll() {
   scrollTop = window.pageYOffset || document.documentElement.scrollTop;
   scrollLeft = window.pageYOffset || document.documentElement.scrollLeft;
@@ -30,11 +42,42 @@ function DisableScroll() {
 function enableScroll() {
   window.onscroll = function () {};
   rootElememt.style.scrollBehavior = "smooth";
-
-  localStorage.setItem("opened", "true");
+  playAudio();
+  // localStorage.setItem("opened", "true");
 }
 
-if (!localStorage.getItem("opened")) {
-  DisableScroll();
+// if (!localStorage.getItem("opened")) {
+//   DisableScroll();
+// }
+DisableScroll();
+
+function playAudio() {
+  song.volume = 0.3;
+  audioIcon.style.display = "flex";
+  song.play();
+  isPlaying = true;
 }
-// DisableScroll();
+
+audioIcon.onclick = function () {
+  if (isPlaying) {
+    song.pause();
+    icon.classList.remove("bi-disc");
+    icon.classList.add("bi-pause-circle");
+  } else {
+    icon.classList.add("bi-disc");
+    icon.classList.remove("bi-pause-circle");
+    song.play();
+  }
+
+  isPlaying = !isPlaying;
+};
+
+// url Data
+const urlParams = new URLSearchParams(window.location.search);
+const nama = urlParams.get("n") || "";
+const pronoun = urlParams.get("p") || "Bapak/Ibu/Saundara/i,";
+
+const namaContainer = document.querySelector(".hero h4 span");
+namaContainer.innerText = `${pronoun} ${nama}`.replace(/ , $/, ",");
+
+document.querySelector("#nama").value = nama;
